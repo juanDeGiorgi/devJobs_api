765cc18a62285a2ffdda1540100ff4fc8cd0846f
unknown <ezequielrango@hotmail.com>
2022-02-05 19:48:20 -0300

    create routes entity jobs

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
 
 const router = express.Router();
-
-const userController = require('../controllers/users');
+const jobs = require('./jobs');
 
 /* entitys routers */
-router.use('/users', userController.getAll);
+
+router.use('./jobs',jobs);
 
 module.exports = router;

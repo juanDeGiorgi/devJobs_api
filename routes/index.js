2852845aa343faca9e86697401f5cbd959db7cb2
unknown <ezequielrango@hotmail.com>
2022-02-09 00:49:19 -0300

    start code base for entity comments

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const jobs = require('./jobs');
-
+const comments = require('./comments');
 /* entitys routers */
 
-router.use('./jobs',jobs);
-
+router.use('/jobs',jobs);
+router.use('/comments,',comments);
 module.exports = router;

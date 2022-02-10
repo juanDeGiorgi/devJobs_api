e0e6c26e27955110e3ee59f2f5bf501cc239d375
Juan De Giorgi <67435938+juanDeGiorgi@users.noreply.github.com>
2022-02-09 21:23:02 -0300

    created companies entity base elements (#4)

---
+++ repositories/companies.js
@@ -0,0 +1,17 @@
+const getAll = () => 'example response';
+
+const getOne = () => 'example response';
+
+const create = () => 'example response';
+
+const update = () => 'example response';
+
+const destroy = () => 'example response';
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  destroy,
+};

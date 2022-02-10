b88a88e07c3f0b49f0aca3b94a8efdea78b44b7f
Ezequiel Rango <79542271+ezequielrango@users.noreply.github.com>
2022-02-09 21:18:09 -0300

    Comments base (#3)
    * create routes entity jobs
    
    * create entity job repositories
    
    * create entity services jobs
    
    * fixed jobService, export methods. Start JobController
    
    * Finish jobController
    
    * start code base for entity comments
    
    * start code base repository for entity comments
    
    * start developing code base services for entity comments
    
    * start developing code base controller for entity comments
    
    Co-authored-by: Alexis Zacre <84038648+Aleex1237@users.noreply.github.com>

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+const jobs = require('./jobs');
+const comments = require('./comments');
 const usersRoutes = require('./users');
 
 /* entitys routers */
 router.use('/users', usersRoutes);
+router.use('/jobs',jobs);
+router.use('/comments,',comments);
 
 module.exports = router;

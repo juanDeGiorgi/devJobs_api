59d0ac71ff230fea3d0cc14f4adb18e66ab576af
Aleex1237 <alexis10893@outlook.com>
2022-02-19 15:53:21 -0300

    apply middleware passport

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 
 const router = express.Router();
+const passport = require('passport');
 
 const jobs = require('./jobs');
 const comments = require('./comments');
 const usersRoutes = require('./users');
 const aplicantsRoutes = require('./aplicants');
 const rols = require('./rols');
+
 /* entitys routers */
 router.use('/users', usersRoutes);
-router.use('/aplicants', aplicantsRoutes);
+router.use('/aplicants', passport.authenticate('jwt', { session: false }), aplicantsRoutes);
 router.use('/jobs', jobs);
 router.use('/comments,', comments);
 router.use('/rols', rols);
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const usersController = require('../controllers/users');
+const authController = require('../controllers/auth');
 
 router.get('/', usersController.getAll);
+router.post('/login', authController.login);
 router.get('/id', usersController.getById);
 router.get('/update', usersController.update);
 router.get('/remove', usersController.remove);

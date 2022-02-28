2cfd1e71956068fd8e9a225e08b9a75da44aaa56
juanDeGiorgi <juanmadegiorgi@gmail.com>
2022-02-28 01:15:57 -0300

    added model for aplicants entity

---
+++ database/models/aplicants.js
@@ -0,0 +1,36 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class aplicants extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      aplicants.belongsTo(models.User, { as: 'user' });
+      aplicants.belongsTo(models.Job, { as: 'job' });
+    }
+  }
+
+  aplicants.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    job_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'aplicants',
+    paranoid: true,
+  });
+  return aplicants;
+};

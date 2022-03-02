892e16538dbcc5d54de2fc64521f1b2be20c8971
Aleex1237 <alexis10893@outlook.com>
2022-03-02 00:35:07 -0300

    roles bases created

--- controllers/rols.js
+++ controllers/rols.js
@@ -1,33 +1,50 @@
-const services = require('../services/rols');
+const rolesService = require('../services/roles');
+const status = require('../constants/statusCodes');
+const messages = require('../constants/messages');
 
 const getById = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const rol = await services.getById(id);
+    const rol = await rolesService.getById(req.params.id);
 
-    res.status(201).json({ rol });
-  } catch (err) {
-    next(err);
+    res.status(status.RESPONSE_OK).json(
+      {
+        status: status.RESPONSE_OK,
+        msg: messages.RESPONSE_OK,
+        rol,
+      },
+    );
+  } catch (error) {
+    next(error);
   }
 };
 
 const create = async (req, res, next) => {
   try {
-    const data = req.body;
-    const newRol = services.create(data);
-    res.status(201).json({ newRol });
-  } catch (err) {
-    next();
+    const newRol = await rolesService.create(req.body.name);
+    res.status(status.RESPONSE_OK_CREATED).json(
+      {
+        status: status.RESPONSE_OK_CREATED,
+        msg: messages.RESPONSE_OK_CREATED,
+        newRol,
+      },
+    );
+  } catch (error) {
+    next(error);
   }
 };
 
 const remove = async (req, res, next) => {
   try {
-    const { id } = req.params;
-    const removeRol = services.remove(id);
-    res.status(201).json({ removeRol });
-  } catch (err) {
-    next(err);
+    await rolesService.remove(req.params.id);
+    res.status(status.RESPONSE_OK_UPDATED).json(
+      {
+        status: status.RESPONSE_OK_UPDATED,
+        msg: messages.RESPONSE_OK_UPDATED,
+        rol: `rol with id: ${req.params.id} has been deleted.`,
+      },
+    );
+  } catch (error) {
+    next(error);
   }
 };
 

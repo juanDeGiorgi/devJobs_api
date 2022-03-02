450f7e7645e60fb4477280639b0f934e3a90bed9
Aleex1237 <alexis10893@outlook.com>
2022-03-02 00:33:58 -0300

    added method post

--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const usersController = require('../controllers/users');
 
 router.get('/', usersController.getAll);
+router.post('/', usersController.create);
 router.get('/id', usersController.getById);
 router.get('/update', usersController.update);
 router.get('/remove', usersController.remove);
